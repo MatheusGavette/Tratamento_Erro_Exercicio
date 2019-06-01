4bb5354aa2d61132ed02015bc4b639521b6647b5
MatheusGavette <matheus.gvtt10@gmail.com>
2019-06-01 12:16:58 -0300

    Criação do exercício com tratamento de erro para validar os saques e o saldo

---
+++ Exercicio01/Entities/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Exercicio01.Entities.Exception;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio01.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account (int number, string holder, double balance, double withDrawLimit)
+        {
+            if (balance < 0 || withDrawLimit < 0)
+            {
+                throw new DomainException("The balance e with draw limit must be great than zero");
+            }
+
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 1)
+            {
+                throw new DomainException("Deposit need to a be great zero");
+            }
+
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Exercicio01.Entities.Exception;
+using Exercicio01.Entities;
+using System.Globalization;
 
 namespace Exercicio01
 {
@@ -6,7 +9,40 @@ namespace Exercicio01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number of account: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder name: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withDrawLimit);
+
+                Console.WriteLine("");
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.WithDraw(amount);
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Error in process: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro in process: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in process: " + e.Message);
+            }
         }
     }
 }
