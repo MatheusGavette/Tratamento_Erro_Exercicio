5fabde295718674346b390d7970dc4a3bc4ea3bb
MatheusGavette <matheus.gvtt10@gmail.com>
2019-06-01 11:52:53 -0300

    Criação do arquivo e conexão com git hub

---
+++ Exercicio01/Entities/Exception/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio01.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
